40869862077efb632171a296dacf1570cf20cc2c
John Blischak <jdblischak@gmail.com>
2019-01-31 17:02:45 -0500

    Improve queries.

--- queries.R
+++ queries.R
@@ -5,10 +5,16 @@ library(dscrutils)
 # area under the curve
 df_auc <- dscquery("soneson2013/",
                   targets = c("de",
+                              "de.samples_per_cond",
                               "analyze",
                               "auc.auc"))
 df_auc <- df_auc[, !grepl("output.file", colnames(df_auc))]
+df_auc <- na.omit(df_auc)
+df_auc$auc.auc <- as.numeric(df_auc$auc.auc)
 print(df_auc)
+print(summary(df_auc$auc.auc))
+tapply(df_auc$auc.auc, df_auc$de, mean)
+tapply(df_auc$auc.auc, df_auc$analyze, mean)
 
 # type I error rate
 df_t1 <- dscquery("soneson2013/",
@@ -17,12 +23,23 @@ df_t1 <- dscquery("soneson2013/",
                               "analyze",
                               "type_one_error.type_one_error"))
 df_t1 <- df_t1[, !grepl("output.file", colnames(df_t1))]
+df_t1 <- na.omit(df_t1)
+df_t1$type_one_error.type_one_error <- as.numeric(df_t1$type_one_error.type_one_error)
 print(df_t1)
+print(summary(df_t1$type_one_error.type_one_error))
+tapply(df_t1$type_one_error.type_one_error, df_t1$zeros, mean)
+tapply(df_t1$type_one_error.type_one_error, df_t1$analyze, mean)
 
 # true FDR
 df_fdr <- dscquery("soneson2013/",
                    targets = c("de",
+                               "de.samples_per_cond",
                                "analyze",
                                "true_fdr.true_fdr"))
 df_fdr <- df_fdr[, !grepl("output.file", colnames(df_fdr))]
+df_fdr <- na.omit(df_fdr)
+df_fdr$true_fdr.true_fdr <- as.numeric(df_fdr$true_fdr.true_fdr)
 print(df_fdr)
+summary(df_fdr$true_fdr.true_fdr)
+tapply(df_fdr$true_fdr.true_fdr, df_fdr$de, mean)
+tapply(df_fdr$true_fdr.true_fdr, df_fdr$analyze, mean)
