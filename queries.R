dc9da442e3ab57d3599ffb7ee8cd517d29d3552c
John Blischak <jdblischak@gmail.com>
2019-03-06 09:32:51 -0500

    Save the query results in a nicer format.

--- queries.R
+++ queries.R
@@ -1,6 +1,10 @@
 #!/usr/bin/env Rscript
 
+library(dplyr)
 library(dscrutils)
+library(stringr)
+
+dir.create("data/", showWarnings = FALSE)
 
 # area under the curve
 df_auc <- dscquery("soneson2013/",
@@ -10,11 +14,18 @@ df_auc <- dscquery("soneson2013/",
                               "auc.auc"))
 df_auc <- df_auc[, !grepl("output.file", colnames(df_auc))]
 df_auc <- na.omit(df_auc)
-df_auc$auc.auc <- as.numeric(df_auc$auc.auc)
+colnames(df_auc) <- c("dsc", "de", "samples_per_cond", "analyze", "auc")
+df_auc$de <- str_replace(df_auc$de, "_data$", "")
+df_auc$auc <- as.numeric(df_auc$auc)
+df_auc[["dsc"]] <- NULL
+
 print(df_auc)
-print(summary(df_auc$auc.auc))
+print(summary(df_auc$auc))
 tapply(df_auc$auc.auc, df_auc$de, mean)
 tapply(df_auc$auc.auc, df_auc$analyze, mean)
+
+write.table(df_auc, file = "data/auc.txt",
+            quote = FALSE, sep = "\t", row.names = FALSE)
 
 # type I error rate
 df_t1 <- dscquery("soneson2013/",
@@ -25,10 +36,15 @@ df_t1 <- dscquery("soneson2013/",
 df_t1 <- df_t1[, !grepl("output.file", colnames(df_t1))]
 df_t1 <- na.omit(df_t1)
 df_t1$type_one_error.type_one_error <- as.numeric(df_t1$type_one_error.type_one_error)
+
+
 print(df_t1)
 print(summary(df_t1$type_one_error.type_one_error))
 tapply(df_t1$type_one_error.type_one_error, df_t1$zeros, mean)
 tapply(df_t1$type_one_error.type_one_error, df_t1$analyze, mean)
+
+write.table(df_t1, file = "data/t1error.txt",
+            quote = FALSE, sep = "\t", row.names = FALSE)
 
 # true FDR
 df_fdr <- dscquery("soneson2013/",
@@ -43,3 +59,6 @@ print(df_fdr)
 summary(df_fdr$true_fdr.true_fdr)
 tapply(df_fdr$true_fdr.true_fdr, df_fdr$de, mean)
 tapply(df_fdr$true_fdr.true_fdr, df_fdr$analyze, mean)
+
+write.table(df_fdr, file = "data/fdr.txt",
+            quote = FALSE, sep = "\t", row.names = FALSE)
