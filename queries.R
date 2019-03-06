053e6e83e615105dd70ec33063106712e0b1bbc5
John Blischak <jdblischak@gmail.com>
2019-03-06 12:29:36 -0500

    Fix queries script.

--- queries.R
+++ queries.R
@@ -1,8 +1,9 @@
 #!/usr/bin/env Rscript
 
-library(dplyr)
-library(dscrutils)
-library(stringr)
+suppressPackageStartupMessages({
+  library(dscrutils)
+  library(stringr)
+})
 
 dir.create("data/", showWarnings = FALSE)
 
@@ -21,8 +22,9 @@ df_auc[["dsc"]] <- NULL
 
 print(df_auc)
 print(summary(df_auc$auc))
-tapply(df_auc$auc.auc, df_auc$de, mean)
-tapply(df_auc$auc.auc, df_auc$analyze, mean)
+tapply(df_auc$auc, df_auc$de, mean, na.rm = TRUE)
+tapply(df_auc$auc, df_auc$samples_per_cond, mean, na.rm = TRUE)
+tapply(df_auc$auc, df_auc$analyze, mean, na.rm = TRUE)
 
 write.table(df_auc, file = "data/auc.txt",
             quote = FALSE, sep = "\t", row.names = FALSE)
@@ -35,13 +37,16 @@ df_t1 <- dscquery("soneson2013/",
                               "type_one_error.type_one_error"))
 df_t1 <- df_t1[, !grepl("output.file", colnames(df_t1))]
 df_t1 <- na.omit(df_t1)
-df_t1$type_one_error.type_one_error <- as.numeric(df_t1$type_one_error.type_one_error)
-
+colnames(df_t1) <- c("dsc", "zeros", "samples_per_cond", "analyze", "type_one_error")
+df_t1$zeros <- str_replace(df_t1$zeros, "_data$", "")
+df_t1$type_one_error <- as.numeric(df_t1$type_one_error)
+df_t1[["dsc"]] <- NULL
 
 print(df_t1)
-print(summary(df_t1$type_one_error.type_one_error))
-tapply(df_t1$type_one_error.type_one_error, df_t1$zeros, mean)
-tapply(df_t1$type_one_error.type_one_error, df_t1$analyze, mean)
+print(summary(df_t1$type_one_error))
+tapply(df_t1$type_one_error, df_t1$zeros, mean, na.rm = TRUE)
+tapply(df_t1$type_one_error, df_t1$samples_per_cond, mean, na.rm = TRUE)
+tapply(df_t1$type_one_error, df_t1$analyze, mean, na.rm = TRUE)
 
 write.table(df_t1, file = "data/t1error.txt",
             quote = FALSE, sep = "\t", row.names = FALSE)
@@ -54,11 +59,16 @@ df_fdr <- dscquery("soneson2013/",
                                "true_fdr.true_fdr"))
 df_fdr <- df_fdr[, !grepl("output.file", colnames(df_fdr))]
 df_fdr <- na.omit(df_fdr)
-df_fdr$true_fdr.true_fdr <- as.numeric(df_fdr$true_fdr.true_fdr)
+colnames(df_fdr) <- c("dsc", "de", "samples_per_cond", "analyze", "true_fdr")
+df_fdr$de <- str_replace(df_fdr$de, "_data$", "")
+df_fdr$true_fdr <- as.numeric(df_fdr$true_fdr)
+df_fdr[["dsc"]] <- NULL
+
 print(df_fdr)
-summary(df_fdr$true_fdr.true_fdr)
-tapply(df_fdr$true_fdr.true_fdr, df_fdr$de, mean)
-tapply(df_fdr$true_fdr.true_fdr, df_fdr$analyze, mean)
+summary(df_fdr$true_fdr)
+tapply(df_fdr$true_fdr, df_fdr$de, mean, na.rm = TRUE)
+tapply(df_fdr$true_fdr, df_fdr$samples_per_cond, mean, na.rm = TRUE)
+tapply(df_fdr$true_fdr, df_fdr$analyze, mean, na.rm = TRUE)
 
 write.table(df_fdr, file = "data/fdr.txt",
             quote = FALSE, sep = "\t", row.names = FALSE)
